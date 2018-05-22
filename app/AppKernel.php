4cbddff4efe5976987cc68ee0075552a179a22b0
Trimech Mehdi <trimechmehdi11@gmail.com>
2018-05-22 10:28:34 +0100

    * bug : disable Doctrine & SwiftMailer

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,8 +18,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+//            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+//            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new App\JiraBundle\AppJiraBundle(),
         ];
